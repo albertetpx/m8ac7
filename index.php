2b144c900ffe73cdae6db14d32afb7ca0a7f7510
albertetpx <104294632+albertetpx@users.noreply.github.com>
2022-11-11 18:26:57 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- <link rel="stylesheet" href="m8ac7.css"> -->
+</head>
+
+<body>
+    <div id="image">
+        <div id="top">
+            <div id="sky"></div>
+        </div>
+        <div id="bottom">
+            <div id="ground"></div>
+        </div>
+        <div id="text">
+            <h1>HELLO WORLD</h1>
+            <h2>
+                <?php
+                 echo apache_get_version()
+                 ?>
+            </h2>
+        </div>
+    </div>
+    <?php echo phpinfo() ?>
+</body>
+
+</html>
