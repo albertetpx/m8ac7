e1e26cdebb5d4bcdbf0dc1516bd8d96695ae6668
albertetpx <aguardiola@etpx.fjaverianas.com>
2022-11-11 17:52:39 +0000

    Change from production server

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link rel="stylesheet" href="m8ac7.css"> -->
+    <link rel="stylesheet" href="m8ac7.css">
 </head>
 
 <body>
@@ -26,7 +26,6 @@
             </h2>
         </div>
     </div>
-    <?php echo phpinfo() ?>
 </body>
 
 </html>
